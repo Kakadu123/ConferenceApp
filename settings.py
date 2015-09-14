a744cb45c55eab0d02c4c8dd4928922dfd963c55
Samuel <samuel.gaspar@att.com>
2015-09-14 20:55:30 +0200

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '568033883113-f1bra7nt8rrkepfu9n434905pjlpn4fr.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'replace with Android client ID'
+IOS_CLIENT_ID = 'replace with iOS client ID'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
